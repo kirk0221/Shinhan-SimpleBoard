7e85966f3e98847892e54cfa2583a37eb746fd85
kirk0221 <99606072+kirk0221@users.noreply.github.com>
2025-05-07 17:34:56 +0900

    lombok 추가

--- src/com/shinhan/SimpleBoard/SimpleBoardDTO.java
+++ src/com/shinhan/SimpleBoard/SimpleBoardDTO.java
@@ -2,6 +2,15 @@ package com.shinhan.SimpleBoard;
 
 import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SimpleBoardDTO {
 	 private int bno;
 	 private String writer;
