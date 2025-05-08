3ffa1329c4103400741b42af2c65108115bf708a
kirk0221 <99606072+kirk0221@users.noreply.github.com>
2025-05-08 12:36:32 +0900

    희찬이가 한건함

--- src/com/shinhan/SimpleBoard/CommonView.java
+++ src/com/shinhan/SimpleBoard/CommonView.java
@@ -12,7 +12,7 @@ public class CommonView {
 
 	public static void ListmanuDisplay() {
 		System.out.println("=================================================================");
-		System.out.println("1. 전체 조회 2. 작성자 조회 3. 제목 조회 4. 내용 조회 5. 작성자/제목 조회 6. 종료");
+		System.out.println("1. 전체 조회 2. 작성자 조회 3. 제목 조회 4. 내용 조회 5. 제목/내용 조회 6. 종료");
 		System.out.println("=================================================================");
 	}
 
