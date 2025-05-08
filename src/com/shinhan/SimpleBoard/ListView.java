053c2ee96ff74b69e268180915b5b52e9512113c
kirk0221 <99606072+kirk0221@users.noreply.github.com>
2025-05-08 12:29:56 +0900

    진짜 최종 수정

--- src/com/shinhan/SimpleBoard/ListView.java
+++
@@ -1,9 +0,0 @@
-package com.shinhan.SimpleBoard;
-
-public class ListView {
-	public static void displaymenu() {
-		System.out.println("=================================================================");
-		System.out.println("1. 전체 조회 2. 작성자 조회 3. 제목 조회 4. 내용 조회 5. 작성자/제목 조회 6. 종료");
-		System.out.println("=================================================================");
-	}
-}
--- src/com/shinhan/SimpleBoard/SimpleBoardDAO.java
+++ src/com/shinhan/SimpleBoard/SimpleBoardDAO.java
@@ -11,99 +11,91 @@ import com.shinhan.utils.DBUtil;
 
 public class SimpleBoardDAO {
 
-	public final String READ_ALL = "select * from SimpleBoard";
-	public final String READ_BY_WRITER = "select * from SimpleBoard where writer like ?";
-	public final String READ_BY_TITLE = "select * from SimpleBoard where title like ?";
-	public final String READ_BY_CONTENTS = "select * from SimpleBoard where contents like ?";
-	public final String READ_BY_TITLE_AND_CONTENTS = "select * from SimpleBoard where title like ? and contents like ?";
-	public final String DELETE= "delete from SimpleBoard where bno = ?";
-	static final String BOARD_UPDATE = """
+	static final String READ_ALL = "select * from SimpleBoard";
+	static final String READ_BY_WRITER = "select * from SimpleBoard where writer like ?";
+	static final String READ_BY_TITLE = "select * from SimpleBoard where title like ?";
+	static final String READ_BY_CONTENTS = "select * from SimpleBoard where contents like ?";
+	static final String READ_BY_TITLE_AND_CONTENTS = "select * from SimpleBoard where title like ? and contents like ?";
+	static final String READ_BY_WRITER_BNO_TITLE = """
+			select *
+			from SimpleBoard
+			where writer = ? and bno = ? and title = ?
+			""";
+	static final String DELETE = "delete from SimpleBoard where bno = ?";
+	static final String UPDATE = """
 			update SimpleBoard
 			set
 			writer = ?,title = ?, contents = ?
 			where bno =?
 			""";
-	static final String BOARD_SELECT = """
-			select *
-			from SimpleBoard
-			where writer = ? and bno = ? and title = ?
+	static final String INSERT = """
+				insert into SimpleBoard(
+			    bno,
+				writer,
+				writedate,
+				title,
+				contents
+				)
+			values( bno.nextval,?,sysdate,?,?)
 			""";
+
 	public int insertPost(SimpleBoardDTO sb) {
 		int result = 0;
 		Connection conn = DBUtil.getConnection();
 		PreparedStatement pst = null;
-		
-		String sql = """
-				insert into SimpleBoard(
-				    bno,
-					writer,
-					writedate,
-					title,
-					contents
-					)
-				values( bno.nextval,?,sysdate,?,?)
-				""";
 		try {
-			pst = conn.prepareStatement(sql);
+			pst = conn.prepareStatement(INSERT);
 			pst.setString(1, sb.getWriter());
 			pst.setString(2, sb.getTitle());
 			pst.setString(3, sb.getContents());
 			result = pst.executeUpdate();
 			conn.commit();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 			try {
 				conn.rollback();
 			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
 			}
 		} finally {
 			DBUtil.dbDisconnect(conn, pst, null);
 		}
-		
+
 		return result;
 	}
-	
-	public boolean deleteBoard(int bno) {
-		
-	    boolean result = false;
-	    Connection conn = null;
-	    PreparedStatement pstmt = null;
-	    ResultSet rs = null;
-	    
-	    try {
-	        conn = DBUtil.getConnection();
-	        pstmt = conn.prepareStatement(DELETE);
-	        pstmt.setInt(1, bno);
-	        
-	        pstmt.executeUpdate();
-	        
-	        conn.commit();
-	        result=true;
-	    } catch (SQLException e) {
-	    	try {
+
+	public int deleteBoard(int bno) {
+		int result = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = DBUtil.getConnection();
+			pstmt = conn.prepareStatement(DELETE);
+			pstmt.setInt(1, bno);
+
+			result = pstmt.executeUpdate();
+
+			conn.commit();
+		} catch (SQLException e) {
+			try {
 				if (conn != null)
-					conn.rollback(); 
+					conn.rollback();
 			} catch (SQLException rollbackEx) {
 				rollbackEx.printStackTrace();
 			}
 			e.printStackTrace();
-	    } finally {
-	    	DBUtil.dbDisconnect(conn, pstmt, rs);
-	    	
-	    }
-	    
-	    return result;
+		} finally {
+			DBUtil.dbDisconnect(conn, pstmt, rs);
+
+		}
+		return result;
 	}
 
-	
 	public int boardUpdate(SimpleBoardDTO board) {
 		Connection conn = DBUtil.getConnection();
-		int resultCount=0;
+		int resultCount = 0;
 		try {
-			PreparedStatement pst = conn.prepareStatement(BOARD_UPDATE);
+			PreparedStatement pst = conn.prepareStatement(UPDATE);
 			pst.setString(1, board.getWriter());
 			pst.setString(2, board.getTitle());
 			pst.setString(3, board.getContents());
@@ -115,41 +107,35 @@ public class SimpleBoardDAO {
 			try {
 				conn.rollback();
 			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
 			}
 		}
-		
+
 		return resultCount;
 
 	}
-	
+
 	public SimpleBoardDTO selectBoard(String writer, int bno, String title) {
 		SimpleBoardDTO board = null;
 		PreparedStatement pst = null;
 		ResultSet rs = null;
 		Connection conn = DBUtil.getConnection();
 		try {
-			pst = conn.prepareStatement(BOARD_SELECT);
+			pst = conn.prepareStatement(READ_BY_WRITER_BNO_TITLE);
 			pst.setString(1, writer);
 			pst.setInt(2, bno);
 			pst.setString(3, title);
 			rs = pst.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				board = makesimpleBoardDTO(rs);
 			}
 		} catch (SQLException e) {
-			e.printStackTrace();
-			System.err.println("==========================");
-			System.err.println("일치하는 정보가 존재하지 않습니다.");
-			System.err.println("==========================");
 		} finally {
 			DBUtil.dbDisconnect(conn, pst, rs);
 		}
 		return board;
 	}
-	
-	public List<SimpleBoardDTO> selectByTitleAndContents(String writer, String contents){
+
+	public List<SimpleBoardDTO> selectByTitleAndContents(String writer, String contents) {
 		List<SimpleBoardDTO> simpleBoardList = new ArrayList<SimpleBoardDTO>();
 		Connection conn = DBUtil.getConnection();
 		PreparedStatement st = null;
@@ -159,21 +145,19 @@ public class SimpleBoardDAO {
 			st.setString(1, "%" + writer + "%");
 			st.setString(2, "%" + contents + "%");
 			rs = st.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				SimpleBoardDTO simpleBoardDTO = makesimpleBoardDTO(rs);
 				simpleBoardList.add(simpleBoardDTO);
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} finally {
 			DBUtil.dbDisconnect(conn, st, rs);
-			
+
 		}
 		return simpleBoardList;
 	}
-	
-	public List<SimpleBoardDTO> selectByContents(String contents){
+
+	public List<SimpleBoardDTO> selectByContents(String contents) {
 		List<SimpleBoardDTO> simpleBoardList = new ArrayList<SimpleBoardDTO>();
 		Connection conn = DBUtil.getConnection();
 		PreparedStatement st = null;
@@ -182,20 +166,18 @@ public class SimpleBoardDAO {
 			st = conn.prepareStatement(READ_BY_CONTENTS);
 			st.setString(1, "%" + contents + "%");
 			rs = st.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				SimpleBoardDTO simpleBoardDTO = makesimpleBoardDTO(rs);
 				simpleBoardList.add(simpleBoardDTO);
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} finally {
 			DBUtil.dbDisconnect(conn, st, rs);
 		}
 		return simpleBoardList;
 	}
-	
-	public List<SimpleBoardDTO> selectByTitle(String title){
+
+	public List<SimpleBoardDTO> selectByTitle(String title) {
 		List<SimpleBoardDTO> simpleBoardList = new ArrayList<SimpleBoardDTO>();
 		Connection conn = DBUtil.getConnection();
 		PreparedStatement st = null;
@@ -204,20 +186,18 @@ public class SimpleBoardDAO {
 			st = conn.prepareStatement(READ_BY_TITLE);
 			st.setString(1, "%" + title + "%");
 			rs = st.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				SimpleBoardDTO simpleBoardDTO = makesimpleBoardDTO(rs);
 				simpleBoardList.add(simpleBoardDTO);
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} finally {
 			DBUtil.dbDisconnect(conn, st, rs);
 		}
 		return simpleBoardList;
 	}
-	
-	public List<SimpleBoardDTO> selectByWriter(String writer){
+
+	public List<SimpleBoardDTO> selectByWriter(String writer) {
 		List<SimpleBoardDTO> simpleBoardList = new ArrayList<SimpleBoardDTO>();
 		Connection conn = DBUtil.getConnection();
 		PreparedStatement st = null;
@@ -226,20 +206,18 @@ public class SimpleBoardDAO {
 			st = conn.prepareStatement(READ_BY_WRITER);
 			st.setString(1, "%" + writer + "%");
 			rs = st.executeQuery();
-			while(rs.next()) {
+			while (rs.next()) {
 				SimpleBoardDTO simpleBoardDTO = makesimpleBoardDTO(rs);
 				simpleBoardList.add(simpleBoardDTO);
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} finally {
 			DBUtil.dbDisconnect(conn, st, rs);
 		}
 		return simpleBoardList;
 	}
-	
-	public List<SimpleBoardDTO> selectAll(){
+
+	public List<SimpleBoardDTO> selectAll() {
 		List<SimpleBoardDTO> simpleBoardList = new ArrayList<SimpleBoardDTO>();
 		Connection conn = DBUtil.getConnection();
 		Statement st = null;
@@ -247,13 +225,11 @@ public class SimpleBoardDAO {
 		try {
 			st = conn.createStatement();
 			rs = st.executeQuery(READ_ALL);
-			while(rs.next()) {
+			while (rs.next()) {
 				SimpleBoardDTO simpleBoardDTO = makesimpleBoardDTO(rs);
 				simpleBoardList.add(simpleBoardDTO);
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} finally {
 			DBUtil.dbDisconnect(conn, st, rs);
 		}
@@ -261,15 +237,10 @@ public class SimpleBoardDAO {
 	}
 
 	private SimpleBoardDTO makesimpleBoardDTO(ResultSet rs) throws SQLException {
-		SimpleBoardDTO simpleBoardDTO = SimpleBoardDTO.builder()
-				.bno(rs.getInt("bno"))
-				.writer(rs.getString("writer"))
-				.writedate(rs.getDate("writedate"))
-				.title(rs.getString("title"))
-				.contents(rs.getString("contents"))
+		SimpleBoardDTO simpleBoardDTO = SimpleBoardDTO.builder().bno(rs.getInt("bno")).writer(rs.getString("writer"))
+				.writedate(rs.getDate("writedate")).title(rs.getString("title")).contents(rs.getString("contents"))
 				.build();
 		return simpleBoardDTO;
 	}
-	
-}
 
+}
