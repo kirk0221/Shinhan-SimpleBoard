f979fead3d938158ad813481a68062ebe3382931
chunhuichan <chc23520@naver.com>
2025-05-08 11:44:39 +0900

    sibal

--- src/com/shinhan/SimpleBoard/SimpleBoardService.java
+++ src/com/shinhan/SimpleBoard/SimpleBoardService.java
@@ -1,5 +1,10 @@
 package com.shinhan.SimpleBoard;
 
 public class SimpleBoardService {
-
+	SimpleBoardDAO simpleboardDAO = new SimpleBoardDAO();
+	
+	public int insertPost(SimpleBoardDTO sb) {
+		return simpleboardDAO.insertPost(sb);
+	}
+	
 }
